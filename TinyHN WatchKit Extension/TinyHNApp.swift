fd3a01e2789a61e7c9fd7458fc27df14c5c0b3e9
Ashwani Kumar <akb762@gmail.com>
2022-03-21 15:31:58 +0530

    Initial Commit

---
+++ TinyHN WatchKit Extension/TinyHNApp.swift
@@ -0,0 +1,19 @@
+//
+//  TinyHNApp.swift
+//  TinyHN WatchKit Extension
+//
+//  Created by Ashwani Kumar on 21/03/22.
+//
+
+import SwiftUI
+
+@main
+struct TinyHNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
